920ce3c9ade759dd1ec20f32ddb0a84df705a3dc
Junu_Choi_laptop <a01081270127@gmail.com>
2024-02-02 14:51:34 -0800

    Sign up Successfully made check Firebase

--- app/src/main/java/com/example/school_project_1/LoginActivity.kt
+++ app/src/main/java/com/example/school_project_1/LoginActivity.kt
@@ -6,34 +6,39 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import com.example.school_project_1.databinding.ActivityLoginBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    private var mAuth = Firebase.auth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val loginButton = findViewById<Button>(R.id.btn_login)
-        val signupButton = findViewById<Button>(R.id.btn_register)
-        val userID = findViewById<EditText>(R.id.edit_id)
-        val userPW = findViewById<EditText>(R.id.edit_pw)
-
-        fun moveToMainPage(){
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
-
-        fun moveToSignUpPage(){
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
-        }
-
-        loginButton.setOnClickListener {
-            Log.d((userID.text.toString()),"잘되네")
+        binding.btnLogin.setOnClickListener {
+            val userID = binding.editId.text.toString().trim()
+            val userPW = binding.editPw.text.toString().trim()
             moveToMainPage()
         }
 
-        signupButton.setOnClickListener {
+        binding.btnRegister.setOnClickListener {
             moveToSignUpPage()
         }
     }
+    private fun moveToMainPage(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun moveToSignUpPage(){
+        val intent = Intent(this, SignUpActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun login(email : String, pwd:String){
+
+    }
+
 }
--- app/src/main/java/com/example/school_project_1/SignUpActivity.kt
+++ app/src/main/java/com/example/school_project_1/SignUpActivity.kt
@@ -2,51 +2,73 @@ package com.example.school_project_1
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.school_project_1.databinding.ActivitySignUpBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
+import com.google.firebase.database.FirebaseDatabase
 import java.util.regex.Pattern
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val password = findViewById<EditText>(R.id.edit_pw)
-        val passwordTwo = findViewById<EditText>(R.id.edit_pw_2)
-        val email = findViewById<EditText>(R.id.edit_id)
-        val message = findViewById<TextView>(R.id.message)
+        mAuth = Firebase.auth
+        val dbRef = FirebaseDatabase.getInstance().reference // Database realtime
+        val message = binding.message
         val pattern: Pattern = Patterns.EMAIL_ADDRESS
-        val signUpBtn = findViewById<Button>(R.id.btn_register)
 
-        fun moveToMainPage(){
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+        binding.btnRegister.setOnClickListener {
+            val pwd = binding.editPw.text.toString()
+            val pwd2 = binding.editPw2.text.toString()
+            val email = binding.editId.text.toString()
+            signUpProcess(email, pwd, pwd2, pattern, message)
+        }
+    }
+    private fun moveToMainPage(){
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+    private fun signUpProcess(email: String,pwd : String, pwd2:String, pattern: Pattern, message: TextView) {
+        if (!pattern.matcher(email).matches()) {
+            //Invalid Email
+            message.text = "Invalid Email Address."
+            return
         }
 
-        fun signUpProcess(){
-            if (pattern.matcher(email.text).matches()) {
-                //Valid Email
-                if (password.text.toString() == passwordTwo.text.toString()){
-                    //Valid Email & Matching Passwords
-                    message.text = "Well Done!"
-                    moveToMainPage()
-                } else {
-                    message.text = "Password mismatching."
-                }
-            } else {
-                //Invalid Email
-                message.text = "Invalid Email Address."
+        if (pwd != pwd2){
+            //Valid Email & Matching Passwords
+            message.text = "Password mismatching."
+            return
+        }
+
+        message.text = "Well Done!"
+        mAuth.createUserWithEmailAndPassword(email,pwd).addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                //pass
+                Toast.makeText(this,"Successfully signed up",Toast.LENGTH_SHORT).show()
+                moveToMainPage()
+            }else{
+                Toast.makeText(this,"Failed to sign up",Toast.LENGTH_SHORT).show()
             }
         }
-
-        signUpBtn.setOnClickListener {
-            signUpProcess()
-        }
-
-
     }
+
 
 }
