dd4081486f9fb95fb927e1da2380730538415859
Junu_Choi_laptop <a01081270127@gmail.com>
2024-02-01 17:58:26 -0800

    First Commit

---
+++ app/src/main/java/com/example/school_project_1/LoginActivity.kt
@@ -0,0 +1,41 @@
+package com.example.school_project_1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<Button>(R.id.btn_login)
+        val signupButton = findViewById<Button>(R.id.btn_register)
+        val userID = findViewById<EditText>(R.id.edit_id)
+        val userPW = findViewById<EditText>(R.id.edit_pw)
+
+        fun moveToMainPage(){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        fun moveToSignUpPage(){
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        loginButton.setOnClickListener {
+            Log.d((userID.text.toString()),"잘되네")
+            moveToMainPage()
+        }
+
+        signupButton.setOnClickListener {
+            moveToSignUpPage()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/school_project_1/SignUpActivity.kt
@@ -0,0 +1,53 @@
+package com.example.school_project_1
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
+import java.util.regex.Pattern
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        val password = findViewById<EditText>(R.id.edit_pw)
+        val passwordTwo = findViewById<EditText>(R.id.edit_pw_2)
+        val email = findViewById<EditText>(R.id.edit_id)
+        val message = findViewById<TextView>(R.id.message)
+        val pattern: Pattern = Patterns.EMAIL_ADDRESS
+        val signUpBtn = findViewById<Button>(R.id.btn_register)
+
+        fun moveToAnotherPage(){
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        fun signUpProcess(){
+            if (pattern.matcher(email.text).matches()) {
+                //Valid Email
+                if (password.text.toString() == passwordTwo.text.toString()){
+                    //Valid Email & Matching Passwords
+                    message.text = "Well Done!"
+                    moveToAnotherPage()
+                } else {
+                    message.text = "Password mismatching."
+                }
+            } else {
+                //Invalid Email
+                message.text = "Invalid Email Address."
+            }
+        }
+
+        signUpBtn.setOnClickListener {
+            signUpProcess()
+        }
+
+    }
+}
