2632da1cb17dcd03b3ed5e9e5cf434647c754bf7
Junu_Choi_laptop <a01081270127@gmail.com>
2024-02-12 14:54:06 -0800

    Dynamically adding views/buttons/layouts from activity_diary.xml on going. Not done yet

--- app/src/main/java/com/example/school_project_1/DiaryActivity.kt
+++ app/src/main/java/com/example/school_project_1/DiaryActivity.kt
@@ -2,7 +2,8 @@ package com.example.school_project_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
 import androidx.core.view.setPadding
@@ -20,7 +21,7 @@ class DiaryActivity : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
     private lateinit var mDbRef: DatabaseReference
     private lateinit var mStorageRef : StorageReference
-    private var mainLayout = binding.linearLayout2
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDiaryBinding.inflate(layoutInflater)
@@ -29,40 +30,77 @@ class DiaryActivity : AppCompatActivity() {
         mDbRef = Firebase.database.reference
         mStorageRef = Firebase.storage.reference
 
-
-//        val dir = filesDir.absolutePath //내부경로의 절대 경로
-//        val filename = "파일이름.txt"
-//        val contentes = "파일 내용\n"
-
         if (Utils.signInCheck(mAuth.currentUser)) {
             setContentView(binding.root)
-        } else{
+        } else {
+            finish()
             Utils.moveToLogInPage(this)
         }
 
-        val btnLayout = LinearLayout(this)
-        val btnDate = Button(this)
-        val btnContent = Button(this)
-
-        btnLayout.setPadding(10)
-        btnLayout.orientation = LinearLayout.HORIZONTAL
-        btnLayout.layoutParams = LinearLayout.LayoutParams(
+        // Create LinearLayout
+        val linearLayout = LinearLayout(this)
+        // Set padding on the LinearLayout
+        linearLayout.setPadding(5, 5, 5, 5)
+        linearLayout.setBackgroundResource(R.drawable.blue_btn)
+        linearLayout.layoutParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.MATCH_PARENT,
-            72
+            LinearLayout.LayoutParams.WRAP_CONTENT
         )
 
-        btnDate.setBackgroundResource(R.drawable.brown_btn)
-        btnDate.gravity = Gravity.CENTER
-        btnDate.layoutParams = LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.MATCH_PARENT
+        // Create a View
+        val view = View(this)
+        val view1 = View(this)
+        val view2 = View(this)
+
+        // Set LayoutParams with weight for the View
+        val params = LinearLayout.LayoutParams(
+            0, // Width
+            ViewGroup.LayoutParams.MATCH_PARENT,   // Height
+            1.0f // Weight
         )
 
-        btnLayout.addView(btnDate)
-        mainLayout.addView(btnLayout)
+        // Create a Button
+        val buttonDate = Button(this)
+        buttonDate.setBackgroundResource(R.drawable.brown_btn)
+        buttonDate.text = "06/27"
+
+        // Set LayoutParams for the ButtonDate
+        val paramsButtonDate = LinearLayout.LayoutParams(
+            0, // Width
+            ViewGroup.LayoutParams.WRAP_CONTENT,   // Height
+            1.0f // Weight
+        )
+        buttonDate.layoutParams = paramsButtonDate
+
+        // Create another Button
+        val buttonText = Button(this)
+        buttonText.setBackgroundResource(R.drawable.brown_btn)
+        buttonText.text = "Hello"
+
+        // Set LayoutParams for the ButtonText
+        val paramsButtonText = LinearLayout.LayoutParams(
+            0, // Width
+            ViewGroup.LayoutParams.WRAP_CONTENT,   // Height
+            4.0f // Weight
+        )
+        buttonText.layoutParams = paramsButtonText
+
+        // Set LayoutParams for the View
+        view.layoutParams = params
+        view1.layoutParams = params
+        params.weight = 0.5f
+        view2.layoutParams = params
+
+        // Add the View and Buttons to the LinearLayout
+        linearLayout.addView(view)
+        linearLayout.addView(buttonDate)
+        linearLayout.addView(view2)
+        linearLayout.addView(buttonText)
+        linearLayout.addView(view1)
+
+        // Add LinearLayout to ScrollView
+        binding.scrollLinear.addView(linearLayout)
 
         binding.backBtn.setOnClickListener { Utils.moveToMainPage(this) }
-
-        //binding.slidersBtn.setOnClickListener { TODO }
     }
 }
